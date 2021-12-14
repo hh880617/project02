7157dda4ebd7c88913afca64ac5af74ee8e5fba3
hh880617 <hh880617@gmail.com>
2021-12-14 14:21:40 +0900

    등록

---
+++ README.md
@@ -0,0 +1,9 @@
+# 웹포폴 프로젝트<br>
+1. 반응형웹
+    - 서브페이지 7개
+    - 웹표준
+    - 웹접근성(상단 GBN)
+    - 시맨틱 태그
+    - mediaquery
+    - bootstrap 활용
+    - font-awesome 활용
---
+++ js/custom.js
@@ -0,0 +1,104 @@
+$(function () {  
+  $(".gnb_wrap > ul > li").bind('mouseenter focusin', function(){
+    $(this).children(".div").stop().show();
+    $(this).addClass("active").siblings().removeClass("active");
+  });
+  $(".gnb_wrap > ul > li").on('mouseleave', function(){
+    $(this).children(".div").stop().hide();
+    $(this).removeClass("active");
+  });
+  $(".gnb_wrap > ul > li").on('focusout', function(){
+    $(this).children(".div").stop().fadeOut(300);
+  });  
+});
+
+// 좌측 모바일 메뉴
+$(document).ready(function () {
+  $(".mobile_menu > ul > li > a").click(function () {
+    if ($(this).parent().children("ul").is(':visible')) {} else { //menu toggle
+      $(".mobile_btn").removeClass("on");
+      $(".mobile_menu > ul > li > ul").hide(200);
+    }
+    $(this).nextAll(".mobile_btn").toggleClass("on");
+    $(this).nextAll(".mobile_menu > ul > li > ul").slideToggle(200);
+  });
+
+  $(".mobile_btn").click(function () {
+    $(this).parent().children("ul").slideToggle(200);
+    $(this).toggleClass("on");
+  });
+
+  $("#mobile_menu_close").click(function () {
+    $("#aside").animate({
+      "left": "-320px"
+    }, 200);
+    $(".close_menu").animate({
+      "left": "-50px"
+    }, 200);
+  });
+
+  $("#mobile_menu_open").click(function () {
+    $("#aside").animate({
+      "left": "0px"
+    }, 200);
+    $(".close_menu").animate({
+      "left": "200px"
+    }, 200);
+  });
+
+});
+// 좌측 모바일 메뉴 끝
+
+
+// Sticky Navi 
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 60) {
+    $("header").addClass("on");
+  } else {
+    $("header").removeClass("on");
+  }  
+}).scroll();
+// Sticky Navi 끝/
+
+
+// 메인 퀵가이드 슬라이드
+$('#banner').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  dots: false,
+  variableWidth: true,
+  centerPadding: '5px',
+  focusOnSelect: false,
+  //variableWidth: true,
+  prevArrow: $('.arrow_banner.prev'),
+  nextArrow: $('.arrow_banner.next'),
+  responsive: [{
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 530,
+      settings: {
+        slidesToShow: 2,
+        //centerMode: true,
+        centerPadding: '5px'
+      }
+    }
+  ]
+});
+
+
+$("#top_btn").on("click", function () {
+  $("html, body").animate({
+    scrollTop: 0
+  }, '500');
+  return false;
+});
